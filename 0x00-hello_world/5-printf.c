45cfd2faa1cba543d75971180eab4c275368b331
JOSESHUN <josepheshun007@gmail.com>
2023-10-05 18:34:32 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,8 @@
  */
 int main(void)
 {
-	printf("proper grammar, but the outcome is a piece of art,\n')
-		return (0);
+printf("proper grammar, but the outcome is a piece of art,\n")
+return (0);
+}
+
 
